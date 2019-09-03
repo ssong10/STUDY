44ad5f80310a02bca36761fd4719d3963818024f
mooyeon-choi <dus1208@ajou.ac.kr>
2019-09-03 23:10:06 +0900

    무연 | 4주차

--- 무연/4주차/bj_5427_불(미완).py
+++ 무연/4주차/bj_5427_불(미완).py
@@ -1,44 +1,49 @@
 import sys
-from collections import deque
+from copy import deepcopy
 I = sys.stdin.readline
 
+def simulation():
+    count = 1
+    answer = 1
+    while count:
+        count = 0
+        past = deepcopy(room)
+        for i in range(h):
+            for j in range(w):
+                if past[i][j] == '@':
+                    if i == 0 or j == 0 or i == h - 1 or j == w - 1:
+                        return answer
+                    count += human(i, j, past)
+        for i in range(h):
+            for j in range(w):
+                if past[i][j] == '*':
+                    fire(i, j, past)
+        answer += 1
+    return 'IMPOSSIBLE'
 
-def bfs(i, j):
+
+def human(i, j, past):
     count = 0
-    cbd[i][j] = '*'
-    esc = deque()
-    esc.append((i, j))
-    while esc:
-        count += 1
-        for _ in range(len(esc)):
-            u, v = esc.popleft()
-            if u == 0 or v == 0 or u == h-1 or v == w-1:
-                return count
-            for n in range(4):
-                if 0 <= u+x[n] < h and 0 <= v+y[n] < w:
-                    if cbd[u+x[n]][v+y[n]] in '.@':
-                        esc.append((u+x[n], v+y[n]))
-                        cbd[u + x[n]][v + y[n]] = '*'
-    return 0xffff
+    for k in range(4):
+        if 0 <= i + x[k] < h and 0 <= j + y[k] < w:
+            if past[i + x[k]][j + y[k]] == '.':
+                room[i + x[k]][j + y[k]] = '@'
+                count += 1
+    return count
+
+
+def fire(i, j, past):
+    for k in range(4):
+        if 0 <= i + x[k] < h and 0 <= j + y[k] < w:
+            if past[i + x[k]][j + y[k]] != '#':
+                room[i + x[k]][j + y[k]] = '*'
 
 
 x = [-1, 1, 0, 0]
 y = [0, 0, -1, 1]
+
 t = int(input())
-for tc in range(t):
-    w, h = map(int, input().split())
-    bd = [list(I()) for _ in range(h)]
-    fire_count = 0
-    human_count = 0
-    for i in range(h):
-        for j in range(w):
-            if bd[i][j] == '*':
-                cbd = [list(line) for line in bd]
-                fire_count = bfs(i, j)
-            if bd[i][j] == '@':
-                cbd = [list(line) for line in bd]
-                human_count = bfs(i, j)
-    if human_count < fire_count:
-        print(human_count)
-    else:
-        print('IMPOSSIBLE')
+for _ in range(t):
+    w, h = map(int, I().split())
+    room = [list(I()) for _ in range(h)]
+    print(simulation())
